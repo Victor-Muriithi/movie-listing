f735d6a44893586aa82d2306723d9dc4816822bf
Victor-Muriithi <vmurithi10@gmail.com>
2022-06-28 15:36:17 +0300

    gh-pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <React.StrictMode>
+  <React.StrictMode basename={window.location.pathname || ''}>
     <Provider store={store}>
     <App />
     </Provider>
